020bd101cb1449323c2ff5c71f3528d0f1ab48e8
ram <ramusus@gmail.com>
2010-12-28 04:46:25 +0300

    added bcc atribute for send_email, send_html_email

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -28,47 +28,49 @@ PRIORITY_MAPPING = {
 
 
 def send_mail(subject, message, from_email, recipient_list, priority="medium",
-              fail_silently=False, auth_user=None, auth_password=None):
+              fail_silently=False, auth_user=None, auth_password=None, bcc=None):
     from django.utils.encoding import force_unicode
     from mailer.models import make_message
-    
+
     priority = PRIORITY_MAPPING[priority]
-    
+
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
     message = force_unicode(message)
-    
+
     make_message(subject=subject,
                  body=message,
                  from_email=from_email,
                  to=recipient_list,
+                 bcc=bcc,
                  priority=priority).save()
     return 1
 
 
 def send_html_mail(subject, message, message_html, from_email, recipient_list,
                    priority="medium", fail_silently=False, auth_user=None,
-                   auth_password=None):
+                   auth_password=None, bcc=None):
     """
     Function to queue HTML e-mails
     """
     from django.utils.encoding import force_unicode
     from django.core.mail import EmailMultiAlternatives
     from mailer.models import make_message
-    
+
     priority = PRIORITY_MAPPING[priority]
-    
+
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
     message = force_unicode(message)
-    
+
     msg = make_message(subject=subject,
                        body=message,
                        from_email=from_email,
                        to=recipient_list,
+                       bcc=bcc,
                        priority=priority)
     email = msg.email
-    email = EmailMultiAlternatives(email.subject, email.body, email.from_email, email.to)
+    email = EmailMultiAlternatives(email.subject, email.body, email.from_email, email.to, email.bcc)
     email.attach_alternative(message_html, "text/html")
     msg.email = email
     msg.save()
@@ -87,7 +89,7 @@ def send_mass_mail(datatuple, fail_silently=False, auth_user=None,
 def mail_admins(subject, message, fail_silently=False, connection=None, priority="medium"):
     from django.conf import settings
     from django.utils.encoding import force_unicode
-    
+
     return send_mail(settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
                      message,
                      settings.SERVER_EMAIL,
@@ -97,7 +99,7 @@ def mail_admins(subject, message, fail_silently=False, connection=None, priority
 def mail_managers(subject, message, fail_silently=False, connection=None, priority="medium"):
     from django.conf import settings
     from django.utils.encoding import force_unicode
-    
+
     return send_mail(settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
                      message,
                      settings.SERVER_EMAIL,
